17fd7ce96e79902c67e5c87d2f3d356a2ba69ced
ALAN_OMAM <omamalan@hotmail.it>
2024-06-12 18:36:46 +0200

    moadificato la tabelle users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('shop_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('shop_address')->nullable();
+            $table->string('shop_phone')->nullable();
+            $table->text('shop_description')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
